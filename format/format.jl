467f8a65932960e24de373c655eba70e21458897
Raphael Sampaio <raphaelaraujosampaio@gmail.com>
2025-08-16 09:56:56 -0300

    Initial commit

---
+++ format/format.jl
@@ -0,0 +1,12 @@
+import Pkg
+Pkg.instantiate()
+
+using JuliaFormatter
+
+if format(dirname(@__FILE__))
+    @info "All files have been formatted!"
+    exit(0)
+else
+    @error "Some files have not been formatted!"
+    exit(1)
+end
