196ac4515555e717facbf55e76512201fd52414d
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-16 11:18:08 -0300

    [skip ci] Update

--- src/Banners.jl
+++ src/Banners.jl
@@ -1,3 +1,20 @@
 module Banners
 
+using FIGlet
+using StyledStrings
+using Colors
+using ColorVectorSpace
+
+include("figlet.jl")
+
+function string_to_matrix(s::AbstractString)
+    lines = split(s, '\n')
+    matrix = [collect(line) for line in lines]
+    return matrix
+end
+
+function matrix_to_string(matrix::Vector{Vector{<:AbstractChar}})
+    return join(map(row -> join(row), matrix), "\n")
+end
+
 end
--- src/figlet.jl
+++ src/figlet.jl
@@ -1,4 +1,4 @@
-function render_text_to_string(text::AbstractString, font::AbstractString)
+function figlet_to_string(text::AbstractString, font::AbstractString)
     io = IOBuffer()
     FIGlet.render(io, text, font)
     return String(take!(io))
